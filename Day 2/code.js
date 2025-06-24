5c246812458e49b0abe1e67ed1bb02476f4dcbb3
vikrant001-git <nitishtiwari.edu@gmail.com>
2025-06-24 16:19:20 +0545

    Day 2 Completed here

---
+++ Day 2/code.js
@@ -0,0 +1,9 @@
+let fullName="John Doe";
+const country="USA";
+let age=25;
+let isLearningJS=true;
+
+console.log(fullName);
+console.log("Country: "+country);
+console.log("Age: "+age);
+console.log("Learning JavaScript? "+ isLearningJS);
---
+++ Day 2/newcode.js
@@ -0,0 +1,11 @@
+let firstname="john";
+const birthyear=1995;
+let isLoggedIn=false;
+
+console.log("hello, "+firstname);
+console.log("Your Birth-Year is: "+birthyear);
+console.log("Login report: "+isLoggedIn);
+
+//Trying to change constant
+
+console.log(birthyear=1965); 
